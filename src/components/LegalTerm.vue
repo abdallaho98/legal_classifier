f1b5b38189c2793653c72adb2f9dc35c63ce3c16
Abdallah Herzallah <fa_herzallah@esi.dz>
2020-02-12 03:23:09 +0100

    type search

--- src/components/LegalTerm.vue
+++ src/components/LegalTerm.vue
@@ -6,7 +6,26 @@
         <div class="right">
             <button @click="stats" class="button" style="vertical-align:middle"><span>الاحصائيات </span></button>
         </div>
-         <input @change="tag" class="search" placeholder="البحث عن طريق كلمات مفتاحية">
+        <div  class="searchbar">
+            <input @change="tag" class="search" placeholder="البحث عن طريق كلمات مفتاحية">
+            <select @change="typeSearch" class="searchtype" name="dropdown">
+                <option value="" selected=true></option>
+                <option value="1">{{type(1)}}</option>
+                <option value="2">{{type(2)}}</option>
+                <option value="3">{{type(3)}}</option>
+                <option value="4">{{type(4)}}</option>
+                <option value="5">{{type(5)}}</option>
+                <option value="6">{{type(6)}}</option>
+                <option value="7">{{type(7)}}</option>
+                <option value="8">{{type(8)}}</option>
+                <option value="9">{{type(9)}}</option>
+                <option value="10">{{type(10)}}</option>
+                <option value="11">{{type(11)}}</option>
+                <option value="12">{{type(12)}}</option>
+                <option value="13">{{type(13)}}</option>
+                <option value="14">{{type(14)}}</option>
+            </select>
+        </div>
         <div  v-bind:key="legal.id" v-for="legal in data">
             <LegalItem v-bind:legal="legal"/>
         </div>
@@ -51,11 +70,32 @@ export default {
             }).catch(err => alert(err))
      } ,
      methods: {
+        callApi: function (tag , type) {
+            if(tag.length > 0 && type.length > 0) {
+                axios.get(`http://localhost:3000/legal/?type=${type}&tag=${tag}`).then((response) => {
+                this.data = response.data.legals ; 
+                }).catch(err => alert(err))
+            } else if(tag.length == 0 && type.length > 0) {
+                axios.get(`http://localhost:3000/legal/?type=${type}`).then((response) => {
+                this.data = response.data.legals ; 
+                }).catch(err => alert(err))
+            } else if(tag.length > 0 && type.length == 0) {
+                axios.get(`http://localhost:3000/legal/?tag=${tag}`).then((response) => {
+                this.data = response.data.legals ; 
+                }).catch(err => alert(err))
+            } else {
+                axios.get(`http://localhost:3000/legal/`).then((response) => {
+                this.data = response.data.legals ; 
+                }).catch(err => alert(err))
+            }
+        },
         tag: function (e) {
-            window.console.log(e.target.value)
-            axios.get(`http://localhost:3000/legal/?tag=${e.target.value}`).then((response) => {
-            this.data = response.data.legals ; 
-            }).catch(err => alert(err))
+            const typeS =  this.$el.querySelector(".searchtype").value
+            this.callApi(e.target.value,typeS)
+        },
+        typeSearch: function (e) {
+            const tagS =  this.$el.querySelector(".search").value
+            this.callApi(tagS , e.target.value)
         },
         reload: function (){
             this.$router.go(0)
@@ -65,7 +105,26 @@ export default {
         },
         stats : function(){
               this.$router.push( { path : '/stats' ,name : 'stats'})
-        }
+        },
+        type: function(i){
+            switch(i){
+                case 1 : return 'القانون المدني';
+                case 2 : return 'قانون الإجراءات المدنية والإدارية';
+                case 3 : return 'القانون التجاري';
+                case 4 : return 'قانون الأسرة';
+                case 5 : return 'قانون الجنسية الجزائرية';
+                case 6 : return 'قانون العقوبات';
+                case 7 : return 'قانون الإجراءات الجزائية';
+                case 8 : return 'قانون تنظيم السجون وإعادة الإدماج الإجتماعي للمحبوسين';
+                case 9 : return 'قانون القضاء العسكري';
+                case 10 : return 'قانون المعاشات العسكرية';
+                case 11 : return 'قانون الانتخابات';
+                case 12 : return 'قانون الجماعات الإقليمية';
+                case 13 : return 'قانون الصفقات العمومية و تفويضات المرفق العام';
+                case 14 : return 'قانون الإعـلام';
+                default : return 'قوانين اخرى';
+            }
+        },
      }
 }
 </script>
@@ -74,16 +133,36 @@ export default {
     .search{
         font-size: 24px;
         color: black;
-        display: inline-block;
-        background-color: white;
+        text-align: right;
+        width: 60%;
+        float: right;
+        padding: 16px 32px 16px 32px;
+        background-color: transparent;
+        border: 0px solid;
+    }
+
+    .searchbar{
         z-index: 7;
         width: 150vh;
+        display: inline-block;
+        background-color: white;
         box-shadow: 0 2px 4px 0 rgba(0,0,0,0.2);
         transition: 0.3s;
         border-radius: 36px; /* 5px rounded corners */ 
-        text-align: right;
-        padding: 16px 32px 16px 32px;
         margin: 32px 32px 32px 32px;
+    }
+
+     .searchtype{
+        margin: 16px 32px 16px 32px;
+        text-align: center;
+        align-items: center;
+        font-size: 24px;
+        color: black;
+        text-align: right;
+        width: 20%;
+        float: left;
+        background-color: transparent;
+        border: 0px solid;
     }
 
     .custom-file-upload {
